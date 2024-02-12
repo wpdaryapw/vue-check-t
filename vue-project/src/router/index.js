e220fc4cdc9be60238b942efa4b5d29126595a4d
Your Name <you@example.com>
2024-02-12 16:13:06 +0300

    Initial commit

---
+++ vue-project/src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // {
+    //   path: '/',
+    //   component: 
+    // },
+    // {
+    //   path: '/about',
+    //   component: 
+    // }
+  ]
+})
+
+export default router
